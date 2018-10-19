117655d64f9d6803deef54be688d0d1e71b38162
Noah Golmant <noah.golmant@gmail.com>
2018-10-19 15:45:29 -0700

    working multi-gpu ray implementation of cifar + resnet

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 """ Top-level directory for all network architectures """
 
-from .cv.resnet import resnet50
+from .cv.resnet import resnet50, resnet20
 from .cv.vgg import vgg16_bn as vgg_16
 
 def build_model(name, **model_params):
