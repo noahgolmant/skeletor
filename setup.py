0ee448b487821eeaae2151c921e328ad655084e2
Noah Golmant <noah.golmant@gmail.com>
2018-10-21 01:52:21 -0700

    working pyptest i install

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 """setup.py for skeletor """
-
-from setuptools import setup
+import setuptools
 
 install_requires = [
     'pyyaml>=3.12',
     'ray>=0.4.0',
-    'awscli',
     'track',
+    'awscli',
 ]
 
 dependency_links = [
@@ -16,19 +15,15 @@ dependency_links = [
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name="skeletor",
-      version="0.1",
-      author="Noah Golmant",
-      author_email="noah.golmant@gmail.com",
-      description="A lightweight module for research experiment reproducibility and analysis",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/noahgolmant/skeletor",
-      license='MIT License',
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: Linux x64",
-      ],
-      install_requires=install_requires,
-      dependency_links=dependency_links)
+setuptools.setup(name="skeletor",
+                 version="0.1",
+                 author="Noah Golmant",
+                 author_email="noah.golmant@gmail.com",
+                 description="A lightweight module for research experiment reproducibility and analysis",
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url="https://github.com/noahgolmant/skeletor",
+                 license='MIT License',
+                 packages=setuptools.find_packages(),
+                 install_requires=install_requires,
+                 dependency_links=dependency_links)
